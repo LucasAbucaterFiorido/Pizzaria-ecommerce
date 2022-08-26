950cc5004fa84a35b27c79c9ff63e7c61e78c4fa
LucasAbucaterFiorido <lucas.abucater@hotmail.com>
2022-08-26 04:03:27 -0300

    feat: introcing astyles

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Ouroborus</title>
+</head>
+<body>
+    <div class="container-fluid teste1">
+        <!-- top -->
+        <div class="container teste2 fonte_teste">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-2">
+                    <!-- logo -->
+                    <a href="#" id="top_logotipo">
+                        <img class="top_logo" src="img/logo/pizzalogo.png" alt="Logotipo Ouroborus">
+                    </a>
+                </div>
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="nav-tabs card-header-tabs">
+                                <a href="#" class="nav-link">
+                                    Cardápio
+                                </a>
+                                <a href="#" class="nav-link">
+                                    Quem Somos ?
+                                </a>
+                                <a href="#" class="nav-link">
+                                    Contato
+                                </a>
+                                <a href="#" class="nav-link">
+                                    Cardápio
+                                </a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <p>Cardápio</p>
+                </div>
+                <div class="col-2">
+                    <!-- <i class="ti-bag"></i> --><p>Carrinho</p>
+                </div>
+                <div class="col-1"></div>
+            </div>
+        </div>
+
+        <div class="container-fluid teste1 d-md-flex text-center">
+            <!-- ****** Descontos do cabeçalho  ****** -->
+            <div class="row">
+                <!-- Area desconto 1 -->
+                <div class="col-4">
+                    <h5>Free Shipping &amp; Returns</h5>
+                    <h6><a href="#">BUY NOW</a></h6>
+                </div>
+                <!-- Area desconto 2 -->
+                <div class="col-4">
+                    <h5>20% Discount for all dresses</h5>
+                    <h6>USE CODE: Colorlib</h6>
+                </div>
+                <!-- Area desconto 3 -->
+                <div class="col-4">
+                    <h5>20% Discount for students</h5>
+                    <h6>USE CODE: Colorlib</h6>
+                </div>
+                <!-- ****** FIM Descontos do cabeçalho ****** -->
+            </div>
+        </div>
+        <div class="container-fluid"></div>
+
+
+
+    </div>    
+
+
+
+
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/jquery-3.6.0.js"></script>
+</body>
+</html>
