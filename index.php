98b092aa1c486cbb96fb16823e8b18f1e246d948
LucasAbucaterFiorido <lucas.abucater@hotmail.com>
2022-08-26 22:27:43 -0300

    feat: new configs offers

--- index.php
+++ index.php
@@ -11,70 +11,78 @@
 <body>
     <div class="container-fluid teste1">
         <!-- top -->
-        <div class="container teste2 fonte_teste">
-            <div class="row">
-                <div class="col-1"></div>
-                <div class="col-2">
-                    <!-- logo -->
-                    <a href="#" id="top_logotipo">
-                        <img class="top_logo" src="img/logo/pizzalogo.png" alt="Logotipo Ouroborus">
-                    </a>
-                </div>
-                <div class="col-4">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="nav-tabs card-header-tabs">
-                                <a href="#" class="nav-link">
-                                    Cardápio
-                                </a>
-                                <a href="#" class="nav-link">
-                                    Quem Somos ?
-                                </a>
-                                <a href="#" class="nav-link">
-                                    Contato
-                                </a>
-                                <a href="#" class="nav-link">
-                                    Cardápio
-                                </a>
-                            </div>
-                            
+        <div class="row">
+            <div class="col-12">
+                <div class="container teste2 fonte_teste">
+                    <div class="row">
+                        <div class="col-1"></div>
+                        <div class="col-2">
+                            <!-- logo -->
+                            <a href="#" id="top_logotipo">
+                                <img class="top_logo" src="img/logo/pizzalogo.png" alt="Logotipo Ouroborus">
+                            </a>
                         </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-header">
+                                    <div class="nav-tabs card-header-tabs">
+                                        <a href="#" class="nav-link">
+                                            Cardápio
+                                        </a>
+                                        <a href="#" class="nav-link">
+                                            Quem Somos ?
+                                        </a>
+                                        <a href="#" class="nav-link">
+                                            Contato
+                                        </a>
+                                        <a href="#" class="nav-link">
+                                            Cardápio
+                                        </a>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <p>Cardápio</p>
+                        </div>
+                        <div class="col-2">
+                            <!-- <i class="ti-bag"></i> --><p>Carrinho</p>
+                        </div>
+                        <div class="col-1"></div>
                     </div>
-                    <p>Cardápio</p>
                 </div>
-                <div class="col-2">
-                    <!-- <i class="ti-bag"></i> --><p>Carrinho</p>
-                </div>
-                <div class="col-1"></div>
             </div>
         </div>
+        <!-- FIM top -->
 
-        <div class="container-fluid teste1 d-md-flex text-center">
-            <!-- ****** Descontos do cabeçalho  ****** -->
-            <div class="row">
-                <!-- Area desconto 1 -->
-                <div class="col-4">
+        <!-- top-Descontos -->
+        <div class="row text-center top_descontos"> <!-- d-md-flex -->
+
+            <!-- Area desconto 1 -->
+            <div class="col-4" style="background-color: gray;">
+                <div class="top_descontos_d1">
                     <h5>Free Shipping &amp; Returns</h5>
                     <h6><a href="#">BUY NOW</a></h6>
                 </div>
-                <!-- Area desconto 2 -->
-                <div class="col-4">
-                    <h5>20% Discount for all dresses</h5>
-                    <h6>USE CODE: Colorlib</h6>
-                </div>
-                <!-- Area desconto 3 -->
-                <div class="col-4">
-                    <h5>20% Discount for students</h5>
-                    <h6>USE CODE: Colorlib</h6>
-                </div>
-                <!-- ****** FIM Descontos do cabeçalho ****** -->
             </div>
+            <!-- Area desconto 2 -->
+            <div class="col-4 top_descontos_d2">
+                <h5>20% Discount for all dresses</h5>
+                <h6>USE CODE: Colorlib</h6>
+            </div>
+            <!-- Area desconto 3 -->
+            <div class="col-4 top_descontos_d3">
+                <h5>20% Discount for students</h5>
+                <h6>USE CODE: Colorlib</h6>
+            </div>
+
         </div>
-        <div class="container-fluid"></div>
-
-
-
+        <!-- FIM top-Descontos -->
     </div>    
+    <div class="container-fluid"></div>
+
+
+
+    
 
 
 
