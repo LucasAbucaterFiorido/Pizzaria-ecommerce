963e9a8ba99c44de825f7524ff48c3f6a5c5505c
LucasAbucaterFiorido <lucas.abucater@hotmail.com>
2022-08-26 23:59:33 -0300

    feat: nav-bar's pizza menu

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css_Slider/core-style.css">
     <title>Ouroborus</title>
 </head>
 <body>
     <div class="container-fluid teste1">
-        <!-- top -->
+        <!-- ****** top ****** -->
         <div class="row">
             <div class="col-12">
                 <div class="container teste2 fonte_teste">
@@ -52,34 +53,81 @@
                 </div>
             </div>
         </div>
-        <!-- FIM top -->
+        <!-- ****** FIM top ****** -->
 
-        <!-- top-Descontos -->
-        <div class="row text-center top_descontos"> <!-- d-md-flex -->
+        <!-- ****** top-Descontos ****** -->
+        <div class="row text-center text-white top_descontos"> <!-- d-md-flex -->
 
             <!-- Area desconto 1 -->
-            <div class="col-4" style="background-color: gray;">
+            <div class="col-4" style="background-color: #b8b8b8;">
                 <div class="top_descontos_d1">
                     <h5>Free Shipping &amp; Returns</h5>
                     <h6><a href="#">BUY NOW</a></h6>
                 </div>
             </div>
             <!-- Area desconto 2 -->
-            <div class="col-4 top_descontos_d2">
-                <h5>20% Discount for all dresses</h5>
-                <h6>USE CODE: Colorlib</h6>
+            <div class="col-4" style="background-color: #ff084e;">
+                <div class="top_descontos_d2">
+                    <h5>20% Discount for all dresses</h5>
+                    <h6>USE CODE: Colorlib</h6>
+                </div>
             </div>
             <!-- Area desconto 3 -->
-            <div class="col-4 top_descontos_d3">
-                <h5>20% Discount for students</h5>
-                <h6>USE CODE: Colorlib</h6>
+            <div class="col-4" style="background-color: #3a3a3a;">
+                <div class="top_descontos_d3">
+                    <h5>20% Discount for students</h5>
+                    <h6>USE CODE: Colorlib</h6>
+                </div>
             </div>
 
         </div>
-        <!-- FIM top-Descontos -->
-    </div>    
-    <div class="container-fluid"></div>
+        <!-- ****** FIM top-Descontos ****** -->
 
+        <!-- ****** Area dos Slides ****** -->
+        <div class="row">
+
+        </div>
+        <!-- ****** FIM Area dos Slides ****** -->
+
+        <!-- ****** Cardápio Pizzas ****** -->
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="text-center">
+                            <h1>Cardápio</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <nav class="navbar navbar-expand justify-content-center">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#" id="filtro_tudo">Tudo</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#" id="filtro_pizza">Pizza</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#" id="filtro_bebidas">Bebidas</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#" id="filtro_promo">Promoções</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>    
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ****** FIM Cardápio Pizzas ****** -->
+    </div>    
 
 
     
