fba5b30e15e71923813c330b71519f208177002f
LucasAbucaterFiorido <lucas.abucater@hotmail.com>
2022-09-24 01:17:52 -0300

    detail

--- Formularios/Login/frm_login.php
+++ Formularios/Login/frm_login.php
@@ -13,7 +13,6 @@
 </head>
 <body class="body_config">
     <?php
-        include_once('../../conexao.php');
         if($_POST)
         {
             //echo "<pre>"; print_r($_POST); echo "</pre>";
