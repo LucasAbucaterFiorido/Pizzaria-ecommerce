dc3bafff470eba26b82fd45aef461516d524a949
LucasAbucaterFiorido <lucas.abucater@hotmail.com>
2022-10-04 22:20:49 -0300

    feat: new visuals

--- index.php
+++ index.php
@@ -21,7 +21,6 @@
             <a href="tel:+5511969708196">
                 <img class="icone-whats" src="img/logo/icons8-whatsapp-160.png" alt="">
             </a>
-            
         </div>    
 
         <?php include_once('index-div/cabecalho-div.php'); ?> <!-- CABEÇALHO -->
@@ -30,7 +29,47 @@
 
         <!-- ****** Area dos Slides ****** -->
         <div class="row">
-            
+            <div class="col-12 p-0">
+                <div id="carouselExampleCaptions" class="carousel slide background-overlay" data-bs-ride="false">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner filtro" style="height: 800px;">
+                        <div class="carousel-item active">
+                            <!-- <div style="background-image: url('img/produtos/pizza6.jpg');"></div> -->
+                            <img src="img/produtos/pizza6.jpg" class="d-block w-100 " alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img/produtos/pizza4.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img/produtos/pizza5.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
         </div>
         <!-- ****** FIM Area dos Slides ****** -->
 
@@ -57,6 +96,7 @@
                                         <!-- <h6 class="titulo_rodape">Copyright ©2022 Todos os direitos reservados | Produzido por Lucas Abucater Fiorido</h6> -->
                                         <span class="titulo_rodape text-center"><h6><u>Venha visitar um dos nossos restaurantes!</u></h6></span>
                                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.7742347815793!2d-46.528414485015645!3d-23.468606764058595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef559cf4ee709%3A0x1717e03b607d1685!2sSenac%20Guarulhos%20-%20Faccini!5e0!3m2!1spt-BR!2sbr!4v1664839434158!5m2!1spt-BR!2sbr" width="100%" height="200px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                        <br><br>
                                     </div>
                                 </div>
                             </div>
@@ -100,6 +140,11 @@
                             <a href=""><i class="icones_rodape fa fa-linkedin" aria-hidden="true"></i></a>
                         </div>
                         <div class="col-4"><!-- coluna visual --></div>
+                        <div class="col-12">
+                            <div><!-- style="position: relative;" -->
+                                <h6 class="titulo_rodape" style="margin: 0px;">Copyright ©2022 Todos os direitos reservados | Produzido por Lucas Abucater Fiorido</h6> <!-- style="position: absolute; margin: 0;" -->
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
